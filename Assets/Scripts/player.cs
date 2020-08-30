4620b821cb8108a7e132677ef538c69d8b903be1
Duncan Andrew <duncanandrew@live.com>
2020-08-30 11:22:02 -0700

    added simple player w/ movement, camera and room

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal_Move") * speed, 0, Input.GetAxis("Vertical_Move") * speed);
+    }
+}
