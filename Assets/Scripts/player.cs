b0580e7565943a74db871a78d82478977520d9f7
Jason Cole <jaymcole2k@gmail.com>
2020-08-31 22:06:15 -0400

    Added rotation to player
    added player based on cursor location

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -17,6 +17,12 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var mouse = Input.mousePosition;
+        var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+        var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(-90, -angle + 180, 0);
+
         rb.velocity = new Vector3(Input.GetAxis("Horizontal_Move") * speed, 0, Input.GetAxis("Vertical_Move") * speed);
     }
 }
