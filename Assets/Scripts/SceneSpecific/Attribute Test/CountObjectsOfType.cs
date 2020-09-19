fb98f758621edff197c029245b1d75b180c8def9
Jason Cole <jaymcole2k@gmail.com>
2020-09-19 10:54:17 -0400

    Added a projectile counter to Attributes Test

---
+++ Assets/Scripts/SceneSpecific/Attribute Test/CountObjectsOfType.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CountObjectsOfType : MonoBehaviour
+{
+
+    public TextMeshProUGUI Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.text = $"Projectiles: {GameObject.FindGameObjectsWithTag("Projectile").Length}";
+        
+    }
+}
